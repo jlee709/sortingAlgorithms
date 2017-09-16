387ceaf23968a1bc1a4c756662473bced3061bf9
Justin Lee <leejwg.3@gmail.com>
2017-09-16 09:17:41 -1000

    added comments on each section of code

--- quick_sort.js
+++ quick_sort.js
@@ -3,31 +3,29 @@
 // implement a quick-sort algorithm 
 console.log("linked");
 
+var list = [50, 55, 20, 30, 10, 15, 70, 80, 95,];
 
-
-function quickSort (list) {
+function quickSort (list, leftSort,rightSort) {
+  // base case recursion test 
   if (!list.length)
-    return arr;
-
-  var pivot = list.splice(0, 1);
-  var left = [];
-  var right = [];
-
-  arr.forEach(function (element) {
-    if (el <= pivot)
-      less.push(el);
+    return list;
+// initiates the splice method to isolate the piviot
+  var pivot = list.splice(0, 1)[0];
+  leftSort = [];
+  rightSort = [];
+// function that will compare e[spliced.list] your value to be compared and pushed into the proper array  
+  list.forEach(function (e){
+    if (e <= pivot)
+      leftSort.push(e);
     else
-      greater.push(el);
+      rightSort.push(e);
   });
   
-  return quickSort(less).concat(pivot, quickSort(greater));
+  return quickSort(leftSort).concat(pivot, quickSort(rightSort));
 }
 
-var testArray = [ 1, 7, 3, 6, 8, 9, 10, 45, 23, 54, 4, 3 ];
-var testArry2 = [50,55,20,30,10,15,70,80,95,];
-var sorted = quickSort(test);
 
-console.log('sorted', sorted);
+console.log(quickSort(list));
 
 
 
